7926be35f1ece5077357bf7a13884fc4ed13f2f7
Moizali4444 <moizali1280@gmail.com>
2024-02-28 23:59:38 +0500

    LWF-02:-update-&-delete-user: edit code complete

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -9,7 +9,9 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <!-- Option 1: Include in HTML -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
+        integrity="sha384-4LISF5TTJX/fLmGSxO53rV4miRxdg84mZsxmO8Rx5jGtp/LbrixFETvWa5a6sESd" crossorigin="anonymous">
     <title>Laravel with Firebase</title>
 </head>
 
