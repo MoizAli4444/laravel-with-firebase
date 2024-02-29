9c7c7dfa881ff112f10c2c6932cafb0af3258291
Moizali4444 <moizali1280@gmail.com>
2024-02-29 23:37:24 +0500

    0

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,16 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\StoreTaskRequest;
-use Illuminate\Support\Facades\Session;
 
 class TaskController extends Controller
 {
-    protected $firestoreDB;
-    public function __construct()
-    {
-        $this->firestoreDB = app('firebase.firestore')->database();
-    }
     /**
      * Display a listing of the resource.
      */
@@ -26,30 +19,15 @@ class TaskController extends Controller
      */
     public function create()
     {
-        // Fetch all documents from the Firestore collection
-        $documents = $this->firestoreDB->collection('users')->documents();
-
-        // Convert Firestore documents to an array
-        $users = [];
-        foreach ($documents as $document) {
-            $users[$document->id()] = $document->data();
-        }
-        return view('pages.task.create', compact('users'));
+        //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreTaskRequest $request)
+    public function store(Request $request)
     {
-        try {
-            $this->firestoreDB->collection('tasks')->add($request->validated());
-            Session::flash('success', 'Task created successfully');
-            return redirect()->route('task.create');
-        } catch (\Exception $e) {
-            Session::flash('error', 'Failed to store task data');
-            return redirect()->back()->withInput();
-        }
+        //
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'task', 'as' => 'task.'], function () {
-    Route::get('/', [TaskController::class, 'index'])->name('list');
-    Route::get('/create', [TaskController::class, 'create'])->name('create');
-    Route::post('/store', [TaskController::class, 'store'])->name('store');
+Route::group(['prefix' => 'task'], function () {
+    Route::get('/', [TaskController::class, 'index'])->name('task.list');
+    Route::get('/create', [TaskController::class, 'create'])->name('task.create');
 });
 
-Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
-    Route::get('/', [UserController::class, 'index'])->name('index');
-    Route::get('/create', [UserController::class, 'create'])->name('create');
-    Route::post('/store', [UserController::class, 'store'])->name('store');
-    Route::get('/edit/{id?}', [UserController::class, 'edit'])->name('edit');
-    Route::put('/update/{id?}', [UserController::class, 'update'])->name('update');
-    Route::delete('/delete/{id?}', [UserController::class, 'destroy'])->name('destroy');
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index'])->name('user.index');
+    Route::get('/create', [UserController::class, 'create'])->name('user.create');
+    Route::post('/store', [UserController::class, 'store'])->name('user.store');
+    Route::get('/edit/{id?}', [UserController::class, 'edit'])->name('user.edit');
+    Route::put('/update/{id?}', [UserController::class, 'update'])->name('user.update');
+    Route::delete('/delete/{id?}', [UserController::class, 'destroy'])->name('user.destroy');
 });
